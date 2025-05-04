b7f84f19765154a19da3d20beb7cd6731d0b114d
waqar ahmad <waqar4704649@gmail.com>
2025-05-04 05:57:46 -0700

    initial release

--- src/utilities/Input.js
+++
@@ -1,3 +0,0 @@
-export const labelStyle = "font-normal text-sm text-black";
-export const inputStyle =
-  "w-full resize-none font-normal text-base text-black rounded py-2.5 px-4 bg-[#f4f4f4] border-none placeholder:text-black placeholder:text-opacity-40 focus:outline-none";
